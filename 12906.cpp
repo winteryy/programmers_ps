edc13af8aa18a854e11b839560208978b0f0c21a
Lee HuiGwang <lhk9805@gmail.com>
2024-11-29 19:14:42 +0900

    pg 12906

---
+++ 12906.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) 
+{
+    vector<int> answer;
+
+    for(auto num: arr) {
+        if(answer.empty()) {
+            answer.push_back(num);
+        }
+    }
+
+    return answer;
+}
