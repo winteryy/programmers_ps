5db2ca4e33ad257f30295571c79f91f743723e55
Lee HuiGwang <lhk9805@gmail.com>
2024-12-01 20:02:00 +0900

    pg 42747

---
+++ 42747.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    sort(citations.begin(), citations.end(), greater<int>());
+
+    int answer = 0;
+    
+    for(int i=0; i<citations.size(); i++) {
+        if(citations[i]>=i+1) {
+            answer = i+1;
+        }
+    }
+    
+    return answer;
+}
